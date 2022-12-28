23d6e8191f44872dbfc8eb45c59dbe74eecc7326
analysteprodez <analysteprodez@gmail.com>
2022-12-28 20:00:32 +0000

    week2 day2 exo1 de cours

---
+++ exocours1.js
@@ -0,0 +1,8 @@
+let age = prompt("Entrez votre age SVP!")
+if (age < 18) {
+    alert("Désolé, vous êtes trop jeune pour conduire cette voiture Éteindre")
+} else if (age = 18) {
+    alert(" Félicitations pour votre première année de conduite.Bonne route ")
+} else(age > 18) {
+    alert("Allumer. Profitez de la balade !")
+}
